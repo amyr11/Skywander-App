4468d9e275397ba8b6ffe48309c7e6b0bd719399
amyr11 <amyrfrancisco11@gmail.com>
2024-04-05 12:05:54 +0800

    chore: setup firebase auth

---
+++ lib/routes.dart
@@ -0,0 +1,15 @@
+// import 'package:go_router/go_router.dart';
+
+// /*
+// This file contains all the routes used in the app. You can add more routes here and delete the /sample route.
+// */
+
+// // The route configuration.
+// final GoRouter router = GoRouter(
+//   routes: <RouteBase>[
+//     GoRoute(
+//       path: '/',
+//       builder: (context, state) => const LoginScreen(),
+//     ),
+//   ],
+// );
