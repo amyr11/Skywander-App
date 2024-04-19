dfd5f5102ac579f3ac5294a47bc8735aecf6f902
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-04-19 13:52:45 +0800

    feat: add User model

---
+++ models/user.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String? id;
+  final String firstName, middleName, lastName, contactNumber;
+  final bool isOnboarded = false;
+
+  User({
+    this.id,
+    required this.firstName,
+    required this.middleName,
+    required this.lastName,
+    required this.contactNumber
+  });
+
+  factory User.fromFirestore(DocumentSnapshot doc) {
+    return User(
+      id: doc.id,
+      firstName: doc['firstName'] as String,
+      middleName: doc['middleName'] as String,
+      lastName: doc['lastName'] as String,
+      contactNumber: doc['contactNumber'] as String,
+    );
+  }
+
+  Map<String, String?> toJSON() {
+    return {
+      'id': id,
+      'firstName': firstName,
+      'middleName': middleName,
+      'lastName': lastName,
+      'contactNumber': contactNumber,
+    };
+  }
+}
