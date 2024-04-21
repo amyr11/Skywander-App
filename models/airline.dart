d5daaec4772f2fc1b78fb473c05b3a39df5c65e5
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-04-21 17:20:59 +0800

    feat: add airline model

---
+++ models/airline.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Airline {
+  // TODO: Add the properties for the Airline model
+  Airline.fromFirestore(DocumentSnapshot doc) {
+    throw UnimplementedError();
+  }
+
+  Map<String, dynamic> toJson() {
+    return {};
+  }
+}
