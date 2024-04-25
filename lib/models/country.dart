fea35b9a9f69d10a86f73fba76dab05c2cad71a2
remoo69 <121857724+remoo69@users.noreply.github.com>
2024-04-25 15:18:40 +0800

    feat: Added Country Model

---
+++ lib/models/country.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Country {
+  final String countryName;
+  final String countryImageUrl;
+
+  Country({
+    required this.countryName,
+    required this.countryImageUrl,
+    });
+
+  factory Country.fromFirestore(DocumentSnapshot doc) {
+    return Country(
+      countryName: doc['countryName'] as String,
+      countryImageUrl: doc['countryImageUrl'] as String,
+    );
+  }
+
+  Map<String, String> toJson() {
+    return {
+      'country_name': countryName,
+      'country_image_url': countryImageUrl,
+    };
+  }
+}
