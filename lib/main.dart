f9b0f65a1295c3f3144a5876cfff41cdcd011d0e
Amyr Francisco <amyrfrancisco11@gmail.com>
2024-05-16 03:26:50 +0800

    fix: change firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
+import 'package:skywander_app/constants.dart';
 import 'package:skywander_app/firebase_options.dart';
 import 'package:skywander_app/routes.dart';
 import 'package:skywander_app/styles.dart';
@@ -8,6 +11,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    GoogleProvider(clientId: GOOGLE_CLIENT_ID),
+  ]);
 
   runApp(const MyApp());
 }
