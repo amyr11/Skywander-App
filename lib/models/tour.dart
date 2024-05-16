659724d31931da7aec77de7340bf2b3b1e040724
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-05-16 20:33:27 +0800

    refactor: modify fields in Tour model

--- lib/models/tour.dart
+++ lib/models/tour.dart
@@ -1,102 +1,94 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:skywander_app/models/destination.dart';
+import 'package:skywander_app/models/subcollections/itenerary.dart';
 import 'subcollections/available_dates.dart';
-import 'subcollections/hotel_option.dart';
 import 'subcollections/review.dart';
 import 'subcollections/tour_highlights.dart';
 import 'theme.dart';
 
 class Tour {
-  final String title, subtitle, imageUrl, airline, origin;
-  final List<String> destinations,
-      tourScope,
-      otherImageUrls,
-      itenerary,
-      inclusions,
-      exclusions;
+  final String title, subtitle, image, airline, origin;
+  final List<Destination> destinations;
+  final List<String> tourScope, otherImages, overviewMarkdown;
   final double basePrice;
   final int days, nights;
-  final Theme theme;
+  final List<Itenerary> itenerary;
+  final List<Theme> theme;
   final List<TourHighlight> tourHighlights;
-  final List<HotelOption> hotelOptions;
   final List<Review> reviews;
   final List<AvailableDates> availableDates;
-  final bool isActive;
+  final bool active;
 
   Tour({
-    required this.title,
-    required this.subtitle,
-    required this.imageUrl,
     required this.airline,
-    required this.origin,
-    required this.destinations,
-    required this.tourScope,
-    required this.basePrice,
-    required this.otherImageUrls,
-    required this.itenerary,
-    required this.inclusions,
-    required this.exclusions,
-    required this.days,
-    required this.nights,
-    required this.theme,
-    required this.tourHighlights,
-    required this.hotelOptions,
-    required this.reviews,
     required this.availableDates,
-    required this.isActive,
+    required this.active,
+    required this.basePrice,
+    required this.days,
+    required this.destinations,
+    required this.image,
+    required this.itenerary,
+    required this.nights,
+    required this.origin,
+    required this.otherImages,
+    required this.overviewMarkdown,
+    required this.reviews,
+    required this.subtitle,
+    required this.tourHighlights,
+    required this.tourScope,
+    required this.theme,
+    required this.title,
   });
 
   factory Tour.fromFirestore(DocumentSnapshot doc) {
     return Tour(
-      title: doc['title'] as String,
-      subtitle: doc['subtitle'] as String,
-      imageUrl: doc['imageUrl'] as String,
       airline: doc['airline'] as String,
-      origin: doc['origin'] as String,
-      tourScope: doc['tourScope'] as List<String>,
-      basePrice: doc['basePrice'] as double,
-      destinations: doc['destinations'] as List<String>,
-      otherImageUrls: doc['otherImageUrls'] as List<String>,
-      inclusions: doc['inclusions'] as List<String>,
-      exclusions: doc['exclusions'] as List<String>,
+      basePrice: doc['base_price'] as double,
+      destinations: doc['destinations'] as List<Destination>,
       days: doc['days'] as int,
-      nights: doc['nights'] as int,
-      theme: Theme.fromFirestore(doc['theme']),
-      itenerary: doc['itenerary'] as List<String>,
-      tourHighlights: (doc['tourHighlights'] as List)
-          .map((e) => TourHighlight.fromFirestore(e))
-          .toList(),
-      hotelOptions: (doc['hotelOptions'] as List)
-          .map((e) => HotelOption.fromFirestore(e))
-          .toList(),
+      image: doc['image'] as String,
+      itenerary: doc['itenerary'] as List<Itenerary>,
+      otherImages: doc['other_images'] as List<String>,
+      origin: doc['origin'] as String,
+      overviewMarkdown: doc['overviewMarkdown'] as List<String>,
       reviews:
           (doc['reviews'] as List).map((e) => Review.fromFirestore(e)).toList(),
+      subtitle: doc['subtitle'] as String,
       availableDates: (doc['availableDates'] as List)
           .map((e) => AvailableDates.fromFirestore(e))
           .toList(),
-      isActive: doc['isActive'] as bool,
+      theme: (List.from(doc['theme'] as List)
+          .map((e) => Theme.fromFirestore(e))).toList(),
+      tourHighlights: (doc['tourHighlights'] as List)
+          .map((e) => TourHighlight.fromFirestore(e))
+          .toList(),
+      tourScope: doc['tour_scope'] as List<String>,
+      active: doc['isActive'] as bool,
+      nights: doc['nights'] as int,
+      title: doc['title'] as String,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'title': title,
-      'subtitle': subtitle,
-      'imageUrl': imageUrl,
-      'tourScope': tourScope,
-      'basePrice': basePrice,
-      'destinations': destinations,
-      'otherImageUrls': otherImageUrls,
-      'inclusions': inclusions,
-      'exclusions': exclusions,
-      'days': days,
-      'nights': nights,
-      'theme': theme.toJson(),
-      'itenerary': itenerary,
-      'tourHighlights': tourHighlights.map((e) => e.toJson()).toList(),
-      'hotelOptions': hotelOptions.map((e) => e.toJson()).toList(),
-      'reviews': reviews.map((e) => e.toJson()).toList(),
+      'airline': airline,
       'availableDates': availableDates.map((e) => e.toJson()).toList(),
-      'isActive': isActive,
+      'basePrice': basePrice,
+      'days': days,
+      'destinations': destinations,
+      'imageUrl': image,
+      'itenerary': itenerary,
+      'isActive': active,
+      'nights': nights,
+      'origin': origin,
+      'otherImageUrls': otherImages,
+      'overviewMarkdown': overviewMarkdown,
+      'reviews': reviews.map((e) => e.toJson()).toList(),
+      'subtitle': subtitle,
+      'theme': theme.map((e) => e.toJson()).toList(),
+      'tourHighlights': tourHighlights.map((e) => e.toJson()).toList(),
+      'tourScope': tourScope,
+      'title': title,
     };
   }
 }
