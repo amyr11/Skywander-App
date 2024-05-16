5b1697dd762771117ec40b32a92fd7da738966dc
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-05-16 08:41:08 +0800

    refactor: added application_form, price, and visa details fields in Visa model

--- lib/models/visa.dart
+++ lib/models/visa.dart
@@ -4,13 +4,23 @@ import 'package:skywander_app/models/country.dart';
 class Visa {
   final Country country;
   final List<dynamic> requirements; // - sinunod ko nalang si gelo
+  final String applicationForm, visaDetails;
+  final double price;
 
-  Visa({required this.country, required this.requirements});
+  Visa(
+      {required this.country,
+      required this.requirements,
+      required this.applicationForm,
+      required this.visaDetails,
+      required this.price});
 
   factory Visa.fromFirestore(DocumentSnapshot doc) {
     return Visa(
       country: Country.fromFirestore(doc['country']),
       requirements: doc['requirements'] as List<dynamic>,
+      applicationForm: doc['applicationForm'] as String,
+      visaDetails: doc['visaDetails'] as String,
+      price: doc['price'] as double,
     );
   }
 
@@ -18,6 +28,9 @@ class Visa {
     return {
       'country': country.toJson(),
       'requirements': requirements, //idk dito wahaha -romeo
+      'applicationForm': applicationForm,
+      'visaDetails': visaDetails,
+      'price': price
     };
   }
 }
