6ba28142a030c4691cd7a9b3b993814661b73f80
remoo69 <121857724+remoo69@users.noreply.github.com>
2024-05-17 03:22:09 +0800

    feat: Added popular (as str)

--- lib/models/destination.dart
+++ lib/models/destination.dart
@@ -1,14 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Destination {
-  final String name, image;
+  final String name, image, popular;
 
-  Destination({required this.name, required this.image});
+  Destination({required this.name, required this.image, required this.popular});
 
   factory Destination.fromFirestore(DocumentSnapshot doc) {
     return Destination(
       name: doc['name'] as String,
       image: doc['image'] as String,
+      popular: doc['popular'] as String, //NOTE: ginawa kong string nalang din para madali sa map HAHAHAHA
     );
   }
 
@@ -16,6 +17,7 @@ class Destination {
     return {
       'name': name,
       'image': image,
+      "popular": popular
     };
   }
 }
