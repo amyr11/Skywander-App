05aa20402ba1fc104423559f52b055053fd3d435
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-05-16 21:06:41 +0800

    refactor: rename fields to match ones in firebase

--- lib/models/hotel.dart
+++ lib/models/hotel.dart
@@ -2,29 +2,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Hotel {
   final String name;
-  final String logoUrl;
+  final String logo;
   final String address;
   final String mapLink;
 
   Hotel(
       {required this.name,
-      required this.logoUrl,
+      required this.logo,
       required this.address,
       required this.mapLink});
 
   factory Hotel.fromFirestore(DocumentSnapshot doc) {
     return Hotel(
       name: doc['name'] as String,
-      logoUrl: doc['logoUrl'] as String,
+      logo: doc['logo'] as String,
       address: doc['address'] as String,
-      mapLink: doc['mapLink'] as String,
+      mapLink: doc['map_link'] as String,
     );
   }
 
   Map<String, String> toJson() {
     return {
       'name': name,
-      'logoUrl': logoUrl,
+      'logo': logo,
       'address': address,
       'mapLink': mapLink,
     };
