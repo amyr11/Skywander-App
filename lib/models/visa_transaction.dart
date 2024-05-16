0d5afc52945f0ff198990b118279a3dd1e82817a
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-05-16 19:34:44 +0800

    refactor: add more fields to VisaTransaction model

--- lib/models/visa_transaction.dart
+++ lib/models/visa_transaction.dart
@@ -3,18 +3,31 @@ import 'package:skywander_app/models/user.dart';
 
 class VisaTransaction {
   final User user;
-  final String
-      status; // di ko na ginawang enum since controlled namna ata natin inputs for status? -romeo
+  final String status, visaFile, paymentMethod;
+  final Map<String, double> fee;
+  final bool paid;
+  final DateTime createdAt, updatedAt;
 
-  VisaTransaction({
-    required this.user,
-    required this.status,
-  });
+  VisaTransaction(
+      {required this.user,
+      required this.status,
+      required this.visaFile,
+      required this.paymentMethod,
+      required this.fee,
+      required this.paid,
+      required this.createdAt,
+      required this.updatedAt});
 
   factory VisaTransaction.fromFirestore(DocumentSnapshot doc) {
     return VisaTransaction(
       user: User.fromFirestore(doc['user']),
       status: doc['status'] as String,
+      visaFile: doc['visa_file'] as String,
+      paymentMethod: doc['payment_method'] as String,
+      fee: doc['fee'] as Map<String, double>,
+      paid: doc['paid'] as bool,
+      createdAt: (doc['created_at'] as Timestamp).toDate(),
+      updatedAt: (doc['updated_at'] as Timestamp).toDate(),
     );
   }
 
@@ -22,6 +35,12 @@ class VisaTransaction {
     return {
       'user': user.toJson(),
       'status': status,
+      'visa_file': visaFile,
+      'payment_method': paymentMethod,
+      'fee': fee,
+      'paid': paid,
+      'created_at': createdAt,
+      'updated_at': updatedAt
     };
   }
 }
