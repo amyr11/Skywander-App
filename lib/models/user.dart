a5694df36daa7bbf0825ce584052a86a56e46985
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-05-16 08:30:24 +0800

    refactor: add email and city fields to User model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
   final String? id;
-  final String firstName, middleName, lastName, contactNumber;
+  final String firstName, middleName, lastName, contactNumber, email, city;
   final bool isOnboarded = false;
 
   User(
@@ -10,16 +10,19 @@ class User {
       required this.firstName,
       required this.middleName,
       required this.lastName,
-      required this.contactNumber});
+      required this.contactNumber,
+      required this.email,
+      required this.city});
 
   factory User.fromFirestore(DocumentSnapshot doc) {
     return User(
-      id: doc.id,
-      firstName: doc['firstName'] as String,
-      middleName: doc['middleName'] as String,
-      lastName: doc['lastName'] as String,
-      contactNumber: doc['contactNumber'] as String,
-    );
+        id: doc.id,
+        firstName: doc['firstName'] as String,
+        middleName: doc['middleName'] as String,
+        lastName: doc['lastName'] as String,
+        contactNumber: doc['contactNumber'] as String,
+        email: doc['email'] as String,
+        city: doc['city'] as String);
   }
 
   Map<String, String?> toJson() {
